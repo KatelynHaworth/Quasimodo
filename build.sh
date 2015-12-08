4942df11f57016bb2cc016dc6a44188c3ef713de
Liam Haworth <liam.haworth@hivemedia.net.au>
2015-12-09 01:33:07 +1100

    Deleted sbt-assembly and the maven as I am switching to sbt-native for package builds

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-version=`git tag`
-release=`git describe --tags --abbrev=5 | gawk -F "-" '{print $2}'`
-commit=`git describe --tags --abbrev=5 | gawk -F "-" '{print $3}'`
-
-mvn package -Dquasimodo.version=$version -Dquasimodo.release=$release -Dquasimodo.commit=$commit
